b83dc7a635f12bdfa540addabf70dbbf1d423c77
umutaev <M8858110177@yandex.com>
2021-04-22 17:07:49 +0500

    Some updates

--- CookIT/searcher.py
+++ CookIT/searcher.py
@@ -1,6 +1,6 @@
 import json
 from google_trans_new import google_translator
-from CookIT.fridge_util import Fridge
+from fridge_util import Fridge
 
 
 class Searcher:
@@ -30,30 +30,11 @@ class Searcher:
 
     def decode2recipe(self):
         recipes_text = []
-        with open("./CookIT/recipes_raw_nosource_fn.json", "r") as json_file:
+        with open("./CookIT/recipes_ru.json", "r") as json_file:
             decoder = json.load(json_file)
             for recipe_code in self.suitable_codes:
                 recipes_text.append(decoder[recipe_code])
         return recipes_text
 
-    def get_suitable_recipes(self, language = "en"):
-        if language=="en":
-            return self.suitable_recipes
-        else:
-            translator = google_translator()
-            translated_recipes = []
-            for recipe in self.suitable_recipes:
-                translated_recipe = {}
-                for key in recipe:
-                    # print(key)
-                    translated_value=None
-                    if type(recipe[key])==str:
-                        translated_value = str(translator.translate(recipe[key], lang_tgt=language))
-                    elif type(recipe[key])==list:
-                        translated_value = [str(translator.translate(i, lang_tgt=language))
-                                            for i in recipe[key]]
-
-                    translated_recipe[key]=translated_value
-                translated_recipes.append(translated_recipe)
-            return translated_recipes
-
+    def get_suitable_recipes(self, language="ru"):
+        return self.suitable_recipes
